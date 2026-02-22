2b8822af90df545e8d9a05ab5fe6d9aa1af6d5f5
Datadisk <ryuji@datadisk.jp>
2026-02-23 06:51:10 +0900

    レイアウトを作って、サイドメニューを固定

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,9 @@
 <template>
-    <div
-        class="min-h-screen bg-black p-10 flex items-center justify-center bg-back-pattern bg-contain bg-center bg-no-repeat">
+    <div>
 
-        <aside class="w-28 bg-transparent text-white p-6">
-            <h2 class="text-xl font-bold mb-8">Menu</h2>
-            <nav class="flex flex-col gap-4">
-                <NuxtLink title="Home" to="/" class="hover:text-emerald-400">ホーム</NuxtLink>
-                <NuxtLink title="About" to="/about" class="hover:text-emerald-400">会社概要</NuxtLink>
-                <NuxtLink title="Services" to="/services" class="hover:text-emerald-400">サービス</NuxtLink>
-            </nav>
-        </aside>
+        
 
-        <main class="flex-1 flexflex-col items-center justify-center bg-transparent p-8 rounded-2xl shadow-xl w-full max-w-md text-center">
+        <main>
             <h1 class="text-3xl font-bold text-emerald-600 mb-4">こんにちは！ らせん</h1>
             <p class="text-slate-600 mb-6">This is the main landing page of the application.</p>
         
